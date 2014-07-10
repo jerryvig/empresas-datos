8220183c8c73605cd0ce2a34495990fcf7929af0
Jerry Vigil <vigilj@google.com>
2014-07-09 17:21:52 -0700

    next commit

--- recoger-morningstar.js
+++ recoger-morningstar.js
@@ -1,50 +1,69 @@
 // http://financials.morningstar.com/ajax/ReportProcess4HtmlAjax.html?&t=XNYS:GPS&region=usa&culture=en-US&cur=USD&reportType=is&period=12&dataType=A&order=asc&columnYear=5&rounding=3&view=raw&r=645954&callback=jsonp1404943419679
 var $ = require('jQuery'),
+	fs = require('fs'),
 	jsdom = require('jsdom'),
 	tickerList = [
-		'GOOG',
 		'AAPL',
-		'MSFT',
-		'INTC',
-		'FB',
-		'COST',
-		'AMZN'
-	];
+		'ADBE',
+		'ADI',
+		'ADP',
+		'ADSK',
+		'AKAM',
+		'ALTR',
+		'ALXN',
+		'AMAT',
+		'AMGN',
+		'AMZN',
+		'ATVI',
+		'AVGO',
+		'BBBY',
+		'BIDU',
+		'BIIB',
+		'BRCM'
+	],
+	outFileName = 'revenueData.csv';
 
-tickerList.forEach(function(ticker, idx) {
-	setTimeout(function(){
-		var params = {
-			t: 'XNAS:' + ticker,
-			region: 'usa',
-			culture: 'en-US',
-			cur: 'USD',
-			reportType: 'is',
-			period: '12',
-			dataType: 'A',
-			order: 'asc',
-			columnYear: '5',
-			rounding: '3',
-			view: 'raw',
-		};
+fs.unlink(outFileName, function(err) {
+	if (err) throw err;
 
-		$.getJSON('http://financials.morningstar.com/ajax/ReportProcess4HtmlAjax.html', params, function(data) {
-			//console.log( 'DATA.RESULT = ' + data.result );
+	tickerList.forEach(function(ticker, idx) {
+		setTimeout(function(){
+			var params = {
+				t: 'XNAS:' + ticker,
+				region: 'usa',
+				culture: 'en-US',
+				cur: 'USD',
+				reportType: 'is',
+				period: '12',
+				dataType: 'A',
+				order: 'asc',
+				columnYear: '5',
+				rounding: '3',
+				view: 'raw',
+			};
 
-			jsdom.env(data.result,
-				['http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js'],
-				function(errors, window) {
-					var revIdx = 0;
-					window.jQuery('div').each(function(i) {
-						if (i > 0 && i < 7 ) {
-							var revenue = window.jQuery(this).text().trim();
-							if (revenue !== 'null') {
-								console.log(revIdx + ',' + ticker + ',' + revenue);
-								revIdx++;
+			$.getJSON('http://financials.morningstar.com/ajax/ReportProcess4HtmlAjax.html', params, function(data) {
+				//console.log( 'DATA.RESULT = ' + data.result );
+
+				jsdom.env(data.result,
+					['http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js'],
+					function(errors, window) {
+						var revIdx = 0;
+						window.jQuery('div').each(function(i) {
+							if (i > 0 && i < 7 ) {
+								var revenue = window.jQuery(this).text().trim();
+								if (revenue !== 'null') {
+									console.log(revIdx + ',' + ticker + ',' + revenue);
+									fs.appendFile(outFileName, revIdx + ',' + ticker + ',' + revenue + '\n', function(){
+										//Callback for file append.
+									});
+									revIdx++;
+								}
 							}
-						}
-					});
-				}
-			);
-		});
-	}, 1000*idx);
+						});
+					}
+				);
+			});
+		}, 1000*idx);
+	});
 });
