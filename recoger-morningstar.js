b2ca083f9fc0c679b41aadc82df146b00a1e5df9
Jerry Vigil <vigilj@google.com>
2015-03-13 16:48:58 -0700

    update from 3/13/2015.

--- recoger-morningstar.js
+++ recoger-morningstar.js
@@ -5,7 +5,8 @@ var $ = require('jQuery'),
 
 const TICKER_LIST_FILE = 'NDX.csv', 
 	OUTPUT_FILE_NAME = 'revenueData.csv',
-	TIME_INTERVAL = 1000;
+	MORNINGSTAR_BASE_URL = 'http://financials.morningstar.com/ajax/ReportProcess4HtmlAjax.html',
+	TIME_INTERVAL = 1200;
 
 fs.readFile(TICKER_LIST_FILE, function(err, data) {
 	var tickerList = (new String(data)).split('\n');
@@ -19,7 +20,6 @@ fs.readFile(TICKER_LIST_FILE, function(err, data) {
 					t: 'XNAS:' + ticker,
 					region: 'usa',
 					culture: 'en-US',
-					cur: 'USD',
 					reportType: 'is',
 					period: '12',
 					dataType: 'A',
@@ -29,26 +29,26 @@ fs.readFile(TICKER_LIST_FILE, function(err, data) {
 					view: 'raw',
 				};
 
-				$.getJSON('http://financials.morningstar.com/ajax/ReportProcess4HtmlAjax.html', params, function(data) {
-					//console.log( 'DATA.RESULT = ' + data.result );
+				$.getJSON(MORNINGSTAR_BASE_URL, params, function(data) {
+					// console.log( 'DATA.RESULT = ' + data.result );
 					console.log('TICKER = ' + ticker);
 
 					jsdom.env(data.result,
-					['http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js'],
-					function(errors, window) {
-						var revIdx = 0;
-						window.jQuery('div').each(function(i) {
-							if (i > 0 && i < 7 ) {
-								var revenue = window.jQuery(this).text().trim();
-								if (revenue !== 'null') {
-									fs.appendFile(OUTPUT_FILE_NAME, revIdx + ',' + ticker + ',' + revenue + '\n', function(){
-										//Callback for file append.
-									});
-									revIdx++;
+						['http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js'],
+						function(errors, window) {
+							var revIdx = 0;
+							window.jQuery('div').each(function(i) {
+								if (i > 0 && i < 7 ) {
+									var revenue = window.jQuery(this).text().trim();
+									if (revenue !== 'null') {
+										fs.appendFile(OUTPUT_FILE_NAME, revIdx + ',' + ticker + ',' + revenue + '\n', function(){
+											//Callback for file append.
+										});
+										revIdx++;
+									}
 								}
-							}
-						});
-					}
+							});
+						}
 					);
 				});
 			}, TIME_INTERVAL*idx);
