c97e22a4d1b46581bc99c3eedbe5063db2cd5f17
Jerry Vigil <vigilj@google.com>
2015-03-18 11:57:56 -0700

    0

--- recoger-morningstar.js
+++ recoger-morningstar.js
@@ -1,7 +1,6 @@
 // http://financials.morningstar.com/ajax/ReportProcess4HtmlAjax.html?&t=XNYS:GPS&region=usa&culture=en-US&cur=USD&reportType=is&period=12&dataType=A&order=asc&columnYear=5&rounding=3&view=raw&r=645954&callback=jsonp1404943419679
 //'use strict';
 
-//WE WANT TO MOVE THIS TO USE CLOSURE TOOLS.
 var goog = require('closure').Closure(),
 	fs = require('fs'),
 	jsdom = require('jsdom');
@@ -82,57 +81,3 @@ RecogerMorningstar.prototype.HandleResponse = function(ticker, e) {
 
 var recogedor = new RecogerMorningstar();
 recogedor.init();
-
-/*
-fs.readFile(TICKER_LIST_FILE, function(err, data) {
-	var tickerList = (new String(data)).split('\n');
-
-	fs.unlink(OUTPUT_FILE_NAME, function(err) {
-		if (err) throw err;
-
-		tickerList.forEach(function(ticker, idx) {
-			setTimeout(function(){
-				var queryParams = {
-					t: 'XNAS:' + ticker,
-					region: 'usa',
-					culture: 'en-US',
-					reportType: 'is',
-					period: '12',
-					dataType: 'A',
-					order: 'asc',
-					columnYear: '5',
-					rounding: '3',
-					view: 'raw',
-				};
-				var uri = new goog.Uri(MORNINGSTAR_BASE_URL);
-				for (var key in queryParams) {
-					uri.setParameterValue(key, queryParams[key]);
-				}
-
-				//$.getJSON(MORNINGSTAR_BASE_URL, params, function(data) {
-				goog.net.XhrIo.send(uri, function(e) {
-					// console.log( 'DATA.RESULT = ' + data.result );
-					console.log('TICKER = ' + ticker);
-					var data = e.target.getResponseJson();
-					jsdom.env(data.result,
-						['http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js'],
-						function(errors, window) {
-							var revIdx = 0;
-							window.jQuery('div').each(function(i) {
-								if (i > 0 && i < 7 ) {
-									var revenue = window.jQuery(this).text().trim();
-									if (revenue !== 'null') {
-										fs.appendFile(OUTPUT_FILE_NAME, revIdx + ',' + ticker + ',' + revenue + '\n', function() {
-											//Callback for file append.
-										});
-										revIdx++;
-									}
-								}
-							});
-						}
-					);
-				});
-			}, TIME_INTERVAL*idx);
-		});
-	});
-}); */
